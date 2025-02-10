23a1687ebec679c4de770490f0058e5ca991c32f
rifqirafialdy <rifqirafialdy@gmail.com>
2025-02-10 13:32:45 +0700

    Arrays-Utils-Feature-2
    Containing feature to remove all duplicate, remove duplicate leaving only one, remove all occurancce with key, and sort the array

--- src/main/java/org/Rifqi/utils/ArraysNumber.java
+++ src/main/java/org/Rifqi/utils/ArraysNumber.java
@@ -1,6 +1,7 @@
 package org.Rifqi.utils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class ArraysNumber {
@@ -26,5 +27,59 @@ public class ArraysNumber {
         }
         return false;
     }
-    
+
+    public List<Integer> removeDuplicate(List<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i).equals(list.get(j))) {
+                    list.remove(i);
+                    j--;
+                }
+            }
+        }
+        return list;
+    }
+
+    public List<Integer> sortArray(List<Integer> list, String sort) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (sort.equalsIgnoreCase("asc") && list.get(i) > list.get(j)) {
+                    int temp = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
+                } else if (sort.equalsIgnoreCase("desc") && list.get(i) < list.get(j)) {
+                    int temp = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
+                }
+            }
+        }
+        return list;
+    }
+
+    public List<Integer> removeAllOccurance(List<Integer> list, int key) {
+        List<Integer> result = new ArrayList<>();
+        for (int number : list) {
+            if (number != key) {
+                result.add(number);
+            }
+        }
+        return result;
+    }
+
+    public List<Integer> removeAllDuplicate(List<Integer> list) {
+        List<Integer> result = new ArrayList<>();
+        HashMap<Integer, Integer> countOccurance = new HashMap<>();
+        for (int num : list) {
+            countOccurance.put(num, countOccurance.getOrDefault(num, 0) + 1);
+        }
+        for (int num : list) {
+            if (countOccurance.get(num) == 1) {
+                result.add(num);
+            }
+        }
+        return result;
+    }
 }
+
+
